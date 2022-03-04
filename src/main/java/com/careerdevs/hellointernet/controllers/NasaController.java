b7efc276cdeac7f2a7e88dd05662cd7957b11358
NickahlisKopel <nickahliskopel.student@careerdevs.com>
2022-03-03 19:56:14 -0500

    completed walkthrough and the date bonus

--- src/main/java/com/careerdevs/hellointernet/controllers/NasaController.java
+++ src/main/java/com/careerdevs/hellointernet/controllers/NasaController.java
@@ -6,13 +6,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+
 @RestController
 @RequestMapping("/nasa")
-public class NasaController {
-    private String myNasaKey = "L6da3GqyJ952ZDnK9RAV04qYAJP3UT5f4f6wyLgh";
-    // private String date = "2021-03-03";
-    private String nasaApodEndpoint = "https://api.nasa.gov/planetary/apod?api_key=" + myNasaKey;
 
+public class NasaController {
+
+    private String myNasaKey = "L6da3GqyJ952ZDnK9RAV04qYAJP3UT5f4f6wyLgh";
+
+    // private String date = "2021-03-03";
+    public String nasaApodEndpoint = "https://api.nasa.gov/planetary/apod?api_key=" + myNasaKey;
     @GetMapping("/apod/{date}")
     public Object apodHandler (RestTemplate restTemplate, @PathVariable("date") String date) {
         Object requestData = restTemplate.getForObject(nasaApodEndpoint + "&date="+date, Object.class);
